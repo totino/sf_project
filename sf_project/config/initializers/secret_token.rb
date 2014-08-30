1e16cafd1b2fd3bf3437b42cdb21046283533abe
totino <s.esposito7782@gmail.com>
2014-08-31 01:39:27 +0200

    restart

---
+++ sf_project/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SfProject::Application.config.secret_token = 'a3b8c0ce283145e0fcd7b95df8fa54c1e01088d3f1e01b93181925a1bb43c7bef6bd5ccc9abaa45100bb2cedc81237a14a9b75fec7b3dbefb52437d350dd6c0f'
